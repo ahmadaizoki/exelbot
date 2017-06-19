e1ed88ed715541edfeaabfab736d21402cbc4922
ahmadaizoki <aizoki.ahmad@gmail.com>
2017-06-19 11:49:07 +0200

    sqfsqfsq

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
-module.exports={
-    APIAI_ACCESS_TOKEN:"5b440dd9729d4f599de38ef1c6f1a648",
-    APIAI_LANG:"fr",
-    APP_ID:"84ee7a15-f9ff-477f-9ef7-496435b70d18",
-    APP_SECRET:"dDyoxh9pDoScyJt5fWcbNd5",
+module.exports = {
+    FB_PAGE_TOKEN: 'EAAb1GFFtPxMBAPHeBduX0pkFWajlR8XmAhZBi2HTs463AarKsBND0di6oZCLZCkNLxF6YW0ipdp0T385VdG5RP5NLp9DMf4ECCRdRRtw0Gyh1ojRKbvowVTqZBfeZBIhYlZAQJXYnduB5XU4Aas6nvC6EjROUnRv93SbL5EpJonAZDZD',
+    FB_VERIFY_TOKEN: 'ahmad@#09450250031991#',
+    API_AI_CLIENT_ACCESS_TOKEN: '5b440dd9729d4f599de38ef1c6f1a648',
+    FB_APP_SECRET: '0d75d416dee94b9d1ea0ce2fb84b0349',
+    SERVER_URL: "https://fbchatb.herokuapp.com/",
     fichier:'Exemple.xlsx',
-}
+};
